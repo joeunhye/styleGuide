c2f4bcbc4f93deb5d602a2c43c589a3ac60eb1df
joeunhye <masimel33@gmail.com>
2024-01-27 22:14:05 +0900

    iframe onload event

--- src/components/ContTab/ContTab.jsx
+++ src/components/ContTab/ContTab.jsx
@@ -1,27 +1,31 @@
-import { useRef, useState } from 'react';
-import styles from './ContTab.module.scss';
-import classNames from 'classnames/bind';
-import Contents from '../Contents/Contents';
-import { useLocation } from 'react-router-dom';
+import { useRef, useState } from "react";
+import styles from "./ContTab.module.scss";
+import classNames from "classnames/bind";
+import Contents from "../Contents/Contents";
+import { useLocation } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 const ContTab = () => {
-    const [currentTab, setCurrentTab] = useState("preview");
-    const location = useLocation();
-    const frameObj = useRef(null);
-    const iframeName = location.pathname.split('/')[1];
+	const [currentTab, setCurrentTab] = useState("preview");
+	// const location = useLocation();
+	// const frameObj = useRef(null);
+	// const iframeName = location.pathname.split('/')[1];
 
-    return (
-        <div className={cx('contTab')}>
-            <div className={cx('tabBtn')}>
-                <button className={cx(currentTab === "preview" && 'on')} onClick={() => setCurrentTab("preview")}>preview</button>
-                <button className={cx(currentTab === "code" && 'on')} onClick={() => setCurrentTab("code")}>code</button>
-            </div>
+	return (
+		<div className={cx("contTab")}>
+			<div className={cx("tabBtn")}>
+				<button className={cx(currentTab === "preview" && "on")} onClick={() => setCurrentTab("preview")}>
+					preview
+				</button>
+				<button className={cx(currentTab === "code" && "on")} onClick={() => setCurrentTab("code")}>
+					code
+				</button>
+			</div>
 
-            <Contents content={currentTab} />
-        </div>
-    );
-}
+			<Contents content={currentTab} />
+		</div>
+	);
+};
 
-export default ContTab
+export default ContTab;
